9d76feb47ae57da620e2a8fafcf66f5571b8d83b
Erik Sabin <eriksabin97@gmail.com>
2024-05-23 07:57:02 -0400

    chore: started working on http service

--- SMFL_CHESS/HTTPService.h
+++ SMFL_CHESS/HTTPService.h
@@ -10,6 +10,17 @@ class HTTPService
 			http.setHost("http://localhost:5052/");
 		}
 
+		void get(std::string uri) {
+			sf::Http::Request request;
+			request.setMethod(sf::Http::Request::Get);
+			request.setUri(uri);
+			request.setHttpVersion(1, 1);
+			request.setField("Content-Type", "application/x-www-form-urlencoded");
+
+
+			sf::Http::Response response = http.sendRequest(request);
+		}
+
 
 };
 
